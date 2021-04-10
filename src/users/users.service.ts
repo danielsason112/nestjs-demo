8173e6e7e607cab2a572f95664f03962081311e1
danielsason112 <danielsason112@gmail.com>
2021-04-10 22:26:47 +0300

    added password hahing with bcrypt

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserRole } from './enums/user-role.enum';
 import { UserEntity } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -12,10 +13,14 @@ export class UsersService {
         private usersRepository: Repository<UserEntity>,
     ) { }
 
+    readonly SALT_ROUNDS: number = 10;
+
     async create(createUserDto: CreateUserDto): Promise<UserEntity> {
         if (await this.usersRepository.findOne(createUserDto.email)) {
             throw new BadRequestException();
         }
+
+        createUserDto.password = await bcrypt.hash(createUserDto.password, this.SALT_ROUNDS);
 
         return this.usersRepository.save({ role: UserRole.SPACTATOR, ...createUserDto });
     }
